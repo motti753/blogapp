8bab2fb609c36eff288e3666dc2834c9e80afa73
望月真紀 <yonetu.s.121@gmail.com>
2024-02-07 07:12:46 +0900

    インスタンス変数埋め込み テンプレートファイル側で表示する

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    # @インスタンス変数
+    @title = "デイトラ"
+    
+    # テンプレート側で「開始タグ <%= @インスタンス変数名 %> 終了タグ」で呼び出す
     render "home/index"
   end
 end
