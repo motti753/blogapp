42183176c9fa0b69d777a0de2080033b878ea0a6
望月真紀 <yonetu.s.121@gmail.com>
2024-02-07 07:56:11 +0900

    ルートURL（http://localhost:3000）にHelperのpathを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # rootは'/'を指す。以下はget '/' => "home#index"と同義
+  root to: "home#index"
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/" => "home#index"
   get "/about" => "home#about"
 
   # Defines the root path route ("/")
