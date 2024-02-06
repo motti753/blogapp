2757b7ff7837ea548576bd20dd254a1ce772ef5e
望月真紀 <yonetu.s.121@gmail.com>
2024-02-07 07:32:21 +0900

    aboutページ作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,13 @@ class HomeController < ApplicationController
   def index
     # @インスタンス変数
     @title = "デイトラ"
-    
+
     # テンプレート側で「開始タグ <%= @インスタンス変数名 %> 終了タグ」で呼び出す
     render "home/index"
+  end
+
+  def about
+    @title = "aboutページです"
+    render "home/about"
   end
 end
