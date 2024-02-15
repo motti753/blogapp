9b2ae772cc540c1cbb2f2bb4b800dc460aa4ea6f
望月真紀 <yonetu.s.121@gmail.com>
2024-02-15 18:37:14 +0900

    CRUD機能のうち、createを実装。 投稿用ページの作成と投稿用ページへの遷移用アイコンを定義。 残りはPOST先のcreateページの実装。

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   root to: "articles#index"
 
   # resources :articles
-  resources :articles,only: [:show]
+  resources :articles,only: [:show, :new, :create]
 end
