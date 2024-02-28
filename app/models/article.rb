a3afed70e9c1bd6b93fba23b97bf0f58f23691b3
望月真紀 <yonetu.s.121@gmail.com>
2024-02-29 08:12:23 +0900

    以下を実施 　・articleテーブルにuserテーブルを参照するuser_idカラムの追加をする migrateを使った追加 　・model/article.rb,user.rbに参照設定を追加する has_many,belong_to 　・article_controller.rbの追加・変更・削除をログインユーザの記事に限定する 　　current_user.articles 　・DBをロールバックし、「user→article」の順番にテーブル作成、ダミーデータを10件追加 　　rails db:rollback STEP=4 ステップ数はmigrateの実行回数分 　　ダミーデータはseed.rbに追加し、rails db:seedで作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,24 +3,35 @@
 # Table name: articles
 #
 #  id         :integer          not null, primary key
-#  content    :text
-#  title      :string
+#  content    :text             not null
+#  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2, maximun: 100},
   format: {with: /\A(?!\@)/}
   # validates :title, length: { minimum: 2}
 
-  validates :content, presence: true, length: {minimum: 10}, uniqueness: true
+  validates :content, presence: true, length: {minimum: 5}, uniqueness: true
   # validates :content, length: {minimum: 10}
 
   # validate 独自のルール 単数系で宣言する
   # validate :validate_title_and_content_length
 
+  belongs_to :user
+
   def display_created_at
     I18n.l(self.created_at, format: :long)
+  end
+
+  def author_name
+    user.display_name
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_26_223421) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_010000) do
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
