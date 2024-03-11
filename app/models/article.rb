a799b4296311277b1f52d130f7384bf1dbe51933
望月真紀 <yonetu.s.121@gmail.com>
2024-03-11 20:12:08 +0900

    eyecatchをActiveStorageを使い、実装。 　・article.rbに「has_one_attached :eyecatch」を追加 　・articles/edit.html.hamlにアイキャッチアップロード用HTMLタグを追加 　・articles/show.html.hamlと_article.html.hamlに「アイキャッチがある場合、表示する」処理を記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -27,6 +27,7 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_one_attached :eyecatch
 
   def display_created_at
     I18n.l(self.created_at, format: :long)
