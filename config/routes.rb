b8c3ea9a8ac639f59ecd8aa3d9dec1c5584c0174
望月真紀 <yonetu.s.121@gmail.com>
2024-03-11 18:48:51 +0900

    LikesのCreateとDeleteを実装。 　・UserテーブルにLikeテーブルにレコードがあるかチェックするhas_liked?メソッドを定義 　・article/show.html.hamlでLikeコントロールのCreateとDestroyを呼び出し 　　　Userテーブルに定義したメソッドの判定結果により、表示する画像を変更する

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :articles do
     # resources :articlesの中に「resources :comments」でURL「articles/id/new」となる
     resources :comments, only: [:new, :create]
+    resource :like, only: [:create, :destroy]
   end
 
   resource :profile, only: [:show, :edit, :update]
