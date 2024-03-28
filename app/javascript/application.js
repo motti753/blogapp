8a5b882d4fc583fb19cdbb20dd1b76e70235d957
望月真紀 <yonetu.s.121@gmail.com>
2024-03-28 19:25:16 +0900

    JavaScriptテスト

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,13 @@ import "controllers"
 
 import "trix"
 import "@rails/actiontext"
+
+// I Had to change:
+import DoISuportIt from 'components/DoISuportIt';
+
+// to (notice the missing `./`)
+import DoISuportIt from './components/DoISuportIt';
+
+document.addEventListener('DOMContentLoaded', () => {
+  window.alert('DOM LOADED')
+})
