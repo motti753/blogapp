c0e80ecdce911cc66b85e644600aa007c9c9792b
望月真紀 <yonetu.s.121@gmail.com>
2024-03-31 09:34:08 +0900

    article/show.html.hamlの表示をJSによって切り替える コントローラからの戻り値(has_liked)がfalseの時はinactive-heartからhiddenクラスを外し、 trueの時はactive-heartからhiddentクラスを外す

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,10 +8,21 @@ import 'axios';
 import jquery from 'jquery'
 window.$ = jquery
 
+// article/show.html.hamlで非表示にしているheartをhas_likedの戻り値によって外す
+const handleHeartDisplay = (hasLiked) => {
+  if (hasLiked) {
+    $('.active-heart').removeClass('hidden')
+  }else {
+    $('.inactive-heart').removeClass('hidden')
+  }
+  console.log(hasLiked)
+}
+
 document.addEventListener('turbo:load', () => {
   const articleId = document.getElementById('article-show').getAttribute('data-article-id')
   axios.get(`/articles/${articleId}/like`)
     .then((response) => {
-      console.log(response)
+      const hasLiked = response.data.hasLiked
+      handleHeartDisplay(hasLiked)
     })
 })
