f8248f47c72c4133ffeaee79c43ace8cd3368233
望月真紀 <yonetu.s.121@gmail.com>
2024-04-02 08:22:44 +0900

    ・comment一覧のCSS修正 ・jsファイルの処理を部品化し、export, importで読み込み

---
+++ app/javascript/modules/handle_heart.js
@@ -0,0 +1,45 @@
+import axios from 'modules/axios';
+import jquery from 'jquery'
+window.$ = jquery
+
+
+// .inactive-heartをクリックしたらcreateメソッドを呼び出し、active-heartを表示させる
+const listenInactiveHeartEvent = (articleId) => {
+  $('.inactive-heart').on('click', () => {
+    axios.post(`/articles/${articleId}/like`)
+      .then((response) => {
+        console.log(response)
+        if (response.data.status == 'OK') {
+          $('.inactive-heart').addClass('hidden')
+          $('.active-heart').removeClass('hidden')
+        }
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
+}
+// .active-heartをクリックしたらdeleteメソッドを呼び出し、inactive-heartを表示させる
+const listenActiveHeartEvent = (articleId) => {
+  $('.active-heart').on('click', () => {
+    axios.delete(`/articles/${articleId}/like`)
+      .then((response) => {
+        if (response.data.status == 'OK') {
+          $('.active-heart').addClass('hidden')
+          $('.inactive-heart').removeClass('hidden')
+        }
+        console.log(response)
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
+}
+
+// 2つのメソッドを他のJSで使えるようにexportする
+export {
+  listenInactiveHeartEvent,
+  listenActiveHeartEvent
+}
