badedca427a4eeb3d692aca38c67f9e159b594b5
望月真紀 <yonetu.s.121@gmail.com>
2024-04-01 20:04:41 +0900

    jsファイルのコードを修正 メソッドに処理をまとめた

--- app/javascript/controllers/article.js
+++ app/javascript/controllers/article.js
@@ -3,6 +3,7 @@ import 'axios';
 import jquery from 'jquery'
 window.$ = jquery
 
+// Token ------------------------------ //
 // axiosのrequest時にheadersにCSRF-Tokenを格納する
 axios.interceptors.request.use((config) => {
   if(['post', 'put', 'patch', 'delete'].includes(config.method)) {
@@ -12,7 +13,9 @@ axios.interceptors.request.use((config) => {
 }, (error) => {
   return Promise.reject(error);
 });
+// Token ------------------------------ //
 
+// like ------------------------------ //
 // article/show.html.hamlで非表示にしているheartをhas_likedの戻り値によって外す
 const handleHeartDisplay = (hasLiked) => {
   if (hasLiked) {
@@ -22,6 +25,20 @@ const handleHeartDisplay = (hasLiked) => {
   }
   console.log(hasLiked)
 }
+// like ------------------------------ //
+
+// comment ------------------------------ //
+const handleCommentForm = () => {
+  $('.show-comment-form').addClass('hidden')
+  $('.comment-text-area').removeClass('hidden')
+}
+
+const appendNewComment = (comment) => {
+  $('.comments-container').append(
+    `<div class='article_comment'><p>${comment.content}</p></div>`
+  )
+}
+// comment ------------------------------ //
 
 document.addEventListener('turbo:load', () => {
   const article = document.getElementById('article-show')
@@ -32,18 +49,34 @@ document.addEventListener('turbo:load', () => {
     .then((response) => {
       const comments = response.data
       comments.forEach((comment) => {
-        $('.comments-container').append(
-          `<div class='article_comment'><p>${comment.content}</p></div>`
-        )
+        appendNewComment(comment)
       })
     })
   
+  // comment ------------------------------ //
   // show-comment-formが押されたら、comment textformを表示させる
   $('.show-comment-form').on('click', () => {
-    $('.show-comment-form').addClass('hidden')
-    $('.comment-text-area').removeClass('hidden')
+    handleCommentForm()
   })
+  // add-comment-buttonが押されたら、comments_controller createを呼び出す
+  $('.add-comment-button').on('click', () => {
+    const content = $('#comment_content').val()
+    if (!content){
+      window.alert('コメントを入力してください')
+    }else {
+      axios.post(`/articles/${articleId}/comments`, {
+        comment: {content: content}
+      })
+        .then((response) => {
+          const comment = response.data
+          appendNewComment(comment)
+          $('#comment_content').val('')
+        })
+    }
+  })
+  // comment ------------------------------ //
 
+  // like ------------------------------ //
   axios.get(`/articles/${articleId}/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked
@@ -79,6 +112,7 @@ document.addEventListener('turbo:load', () => {
         console.log(e)
       })
   })
+  // like ------------------------------ //
 })
 
 // import applyCaseMiddleware from "axios-case-converter";
