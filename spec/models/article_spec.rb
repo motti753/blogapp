d5de10286fcc823b026aebac8b548b9ae7c75343
望月真紀 <yonetu.s.121@gmail.com>
2024-04-11 08:05:34 +0900

    article_modelのテスト 正常終了

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'タイトルと内容が入力されていれば、記事を保存できる' do
+    user = User.create!({
+      email: 'aaaaaa@gmail.com',
+      password: 'password'
+    })
+
+    article = user.articles.build({
+      title: Faker::Games::Pokemon.name,
+      content: Faker::Games::Pokemon.location
+    })
+    # expect=期待する articleがto be_valid=保存できること
+    expect(article).to be_valid
+  end
 end
