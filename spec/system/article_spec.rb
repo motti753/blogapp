69053ad19702caa8ee2577858bc87fc64fb261d6
望月真紀 <yonetu.s.121@gmail.com>
2024-04-15 07:30:50 +0900

    user, articleのダミーデータを作成し、 ブラウザ上でarticle.titleが表示されているか確認 ※visitの表示は一瞬のため、長期間表示されるように修正必要かも

--- spec/system/article_spec.rb
+++ spec/system/article_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 require 'capybara/rails'
 
 RSpec.describe 'Article', type: :system do
+  let!(:user) { create(:user) }
+  let!(:articles) { create_list(:article, 3, user: user) }
+
   it '記事一覧が表示される' do
     visit root_path
+    artilces.each do |article|
+      # pageにartilce最初のタイトルが存在しているか
+    expect(page).to have_content(artilce.title)
+    end
   end
 end
